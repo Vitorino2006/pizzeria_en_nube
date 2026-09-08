2d46a2106ff0396a7d8996a78244368eaa7a2d24
Vitorino2006 <matigallinatti@gmail.com>
2026-09-08 15:09:36 -0400

    Cambios para el merge

--- conteo.py
+++ conteo.py
@@ -1,5 +1,6 @@
 Clientes_lunes = 128
 Clientes_martes = 128
+#El modelo funciona muy bien
 #Estamos contruyendo un modelo de prediccion
 Cliesntes_miercoles = (Clientes_lunes + Clientes_martes)
 clientes_sabado = 2

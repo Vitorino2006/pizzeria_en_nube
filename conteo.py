33951e1adc5c6a11702ed1f99f4cbdadc8871f4a
Vitorino2006 <matigallinatti@gmail.com>
2026-09-08 15:15:21 -0400

    Agregar archivo extraccion

--- conteo.py
+++ conteo.py
@@ -2,6 +2,6 @@ Clientes_lunes = 128
 Clientes_martes = 128
 #El modelo funciona muy bien
 #Estamos contruyendo un modelo de prediccion
-Cliesntes_miercoles = (Clientes_lunes + Clientes_martes)vo
+Cliesntes_miercoles = (Clientes_lunes + Clientes_martes)
 clientes_sabado = 2
 345
